53ee2a9ae01871fe3d06cd4340f3df5cc5a9ee2b
Beatriz-Riby4320 <beatriz29851@estudante.ifb.edu.br>
2025-07-30 10:49:16 -0300

    criado com o segundo usuário

--- POO-Usuário/src/App.tsx
+++ POO-Usuário/src/App.tsx
@@ -3,11 +3,15 @@ import { Usuario } from './models/Usuario'
 import './App.css'
 
 function App() {
-  const usuario = new Usuario('João', 30);
+  const usuario1 = new Usuario('João', 30);
+  const usuario2 = new Usuario('Maria', 25);
+
+ 
   
   return (
     <>
-      <p>{usuario.apresentar()}</p>
+      <p>{usuario1.apresentar()}</p>
+      <p>{usuario2.apresentar()}</p>
     </>
   )
 }
